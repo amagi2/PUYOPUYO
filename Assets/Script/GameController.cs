ae883d41c1f5a0b2c75091af26ca03a0b6f3fb0b
DESKTOP-TRSH61K\akage <akagekaga@gmail.com>
2022-07-01 22:25:17 +0900

    Unity作成。

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    int Main_Colour;
+    int Sab_Colour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Rand_Colour()
+    {
+        //スイッチ文でメインカラーとサブカラーを決める
+    }
+
+    void Spawn()
+    {
+        //メインカラーとサブカラーをもとにスポーン
+    }
+}
